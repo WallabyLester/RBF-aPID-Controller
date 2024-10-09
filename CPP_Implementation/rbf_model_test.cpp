1a15f75dbf0d677cec4172040008df51c29bd197
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-08 21:55:08 -0700

    Added test of prediction.

--- CPP_Implementation/rbf_model_test.cpp
+++ CPP_Implementation/rbf_model_test.cpp
@@ -5,8 +5,8 @@
 class RBFModelTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        // Set up an RBF model with 3 centers, 3D input, and random centers
-        n_centers = 3;
+        // Set up an RBF model with 5 centers, 3D input, and random centers
+        n_centers = 5;
         input_dim = 3;
         rbf = new RBFModel(n_centers, input_dim);
     }
@@ -25,6 +25,18 @@ TEST_F(RBFModelTest, Constructor_And_Initial_Weights) {
     for (int i = 0; i < n_centers; ++i) {
         EXPECT_EQ(rbf->get_weight(i), 0.0);
     }
+}
+
+// Test predict method
+TEST_F(RBFModelTest, Predict_Output) {
+    // Set some weights and define centers
+    for (int i = 0; i < n_centers; ++i) {
+        rbf->set_weight(i, 1.0);
+    }
+
+    double input[] = {0.5, 0.5, 0.5};
+    double output = rbf->predict(input);
+    EXPECT_GT(output, 0.0); 
 }
 
 // Main function for running tests
