276f1143d9e9106c52cadd60067839b43dc50fb7
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-09 19:59:53 -0700

    Setting up aPID controller tests.

--- TF_Implementation/test/test_aPID_tf.py
+++ TF_Implementation/test/test_aPID_tf.py
@@ -4,5 +4,17 @@ import tensorflow as tf
 from aPID_tf import AdaptivePIDTf
 from RBF_tf import RBFAdaptiveModel
 
+class TestAdaptivePIDTf(unittest.TestCase):
+    def setUp(self):
+        """ Set up RBFAdaptiveModel and AdaptivePIDTf class instances."""
+        self.Kp = 7.0
+        self.Ki = 0.5
+        self.Kd = 0.01
+        self.n_centers = 5
+        self.input_dim = 3
+        self.rbf_model = RBFAdaptiveModel(self.n_centers, self.input_dim)
+        self.pid_controller = AdaptivePIDTf(self.Kp, self.Ki, self.Kd, self.rbf_model)
+
+
 if __name__ == '__main__':
     unittest.main()
