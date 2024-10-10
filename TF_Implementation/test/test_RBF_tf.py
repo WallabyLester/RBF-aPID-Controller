5f42375fe474b0dad4885b81bf8d023a27743f4a
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-09 17:22:09 -0700

    Added testing to TF implementation.

---
+++ TF_Implementation/test/test_RBF_tf.py
@@ -0,0 +1,8 @@
+import unittest
+import numpy as np
+import tensorflow as tf
+from RBF_tf import RBFLayer, RBFAdaptiveModel, train_rbf_adaptive
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ TF_Implementation/test/test_aPID_tf.py
@@ -0,0 +1,8 @@
+import unittest
+import numpy as np
+import tensorflow as tf
+from aPID_tf import AdaptivePIDTf
+from RBF_tf import RBFAdaptiveModel
+
+if __name__ == '__main__':
+    unittest.main()
